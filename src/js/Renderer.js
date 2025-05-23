2bdc8b5351c7eca22c53fe8aa544a51a5b378091
VoidArc Studio <chudybyczek2137@gmail.com>
2025-05-23 22:33:22 +0200

    Update and rename render.js to Renderer.js

--- src/js/Renderer.js
+++ src/js/Renderer.js
@@ -1,7 +1,14 @@
-const fs = require('fs');
+const { ipcRenderer } = require('electron');
 
 document.getElementById('config-form').addEventListener('submit', (event) => {
     event.preventDefault();
+    
+    // Validate inputs
+    const maxFps = document.getElementById('max_fps').value;
+    if (maxFps < 30 || maxFps > 240) {
+        alert('Max FPS must be between 30 and 240');
+        return;
+    }
     
     const config = `
 [General]
@@ -12,7 +19,7 @@ fullscreen=${document.getElementById('fullscreen').checked}
 [Rendering]
 backend=${document.getElementById('backend').value}
 vsync=${document.getElementById('vsync').checked}
-max_fps=${document.getElementById('max_fps').value}
+max_fps=${maxFps}
 
 [GPU]
 vendor=${document.getElementById('vendor').value}
@@ -20,6 +27,17 @@ opengl_version=${document.getElementById('opengl_version').value}
 vulkan_version=${document.getElementById('vulkan_version').value}
     `;
     
-    fs.writeFileSync('../config/gameframe.conf', config);
+    // Save to default config file
+    require('fs').writeFileSync('../config/gameframe.conf', config);
     alert('Configuration saved!');
+});
+
+// Handle launch button
+document.getElementById('launch-gameframe').addEventListener('click', () => {
+    ipcRenderer.send('launch-gameframe');
+});
+
+// Display launch result
+ipcRenderer.on('launch-result', (event, message) => {
+    alert(message);
 });

878d15af672c5aef1818c018833f69383f1a3337
VoidArc Studio <chudybyczek2137@gmail.com>
2025-05-23 22:50:08 +0200

    Update frame_timer.hpp

--- src/cpp/frame_timer.hpp
+++ src/cpp/frame_timer.hpp
@@ -1,8 +1,13 @@
 #ifndef FRAME_TIMER_HPP
 #define FRAME_TIMER_HPP
 
+#include <cstdint>
+
 extern "C" {
-    void start_frame_timer(unsigned int max_fps);
+    void start_frame_timer(uint32_t max_fps, uint32_t refresh_rate);
+    void stop_frame_timer();
+    uint64_t get_frame_count();
+    double get_average_frame_time();
 }
 
 #endif

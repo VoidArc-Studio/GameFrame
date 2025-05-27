556374e86d0d210c739b47a25abccca2f43f20bd
VoidArc Studio <chudybyczek2137@gmail.com>
2025-05-27 13:18:19 +0200

    launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
+# Domyślne wartości
+RESOLUTION="1920x1080"
+QUALITY="4k"
+OPTIONS=""
+APP=""
+
 # Parsowanie argumentów
-SCALING="FSR"
-FPS="60"
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --scaling) SCALING="$2"; shift ;;
-        --fps) FPS="$2"; shift ;;
+        [0-9]*x[0-9]*) RESOLUTION="$1" ;;
+        4k|high|low) QUALITY="$1" ;;
+        ++) OPTIONS="++" ;;
+        +vk|+hud|+gm) OPTIONS="$OPTIONS $1" ;;
+        *) APP="$*" ; break ;;
     esac
     shift
 done
 
-# Uruchomienie GameFrame z vkBasalt, MangoHud, GameMode
-MANGOHUD=1 ENABLE_VKBASALT=1 gamemoderun gameframe -W 1920 -H 1080 -r $FPS --scaling $SCALING -- steam
+# Walidacja rozdzielczości
+if ! [[ $RESOLUTION =~ ^[0-9]+x[0-9]+$ ]]; then
+    echo "Błędny format rozdzielczości. Oczekiwano WxH, np. 1920x1080"
+    exit 1
+fi
+
+# Ustawienie zmiennych środowiskowych
+if [[ $OPTIONS == *"++"* || $OPTIONS == *"+vk"* ]]; then
+    export ENABLE_VKBASALT=1
+    export VKBASALT_CONFIG_FILE=~/.config/gameframe/vkbasalt.conf
+fi
+if [[ $OPTIONS == *"++"* || $OPTIONS == *"+hud"* ]]; then
+    export MANGOHUD=1
+    export MANGOHUD_CONFIG_FILE=~/.config/gameframe/mangohud.conf
+fi
+
+# Uruchomienie GameFrame
+if [[ $OPTIONS == *"++"* || $OPTIONS == *"+gm"* ]]; then
+    gamemoderun gameframe $RESOLUTION $QUALITY "$OPTIONS" "$APP"
+else
+    gameframe $RESOLUTION $QUALITY "$OPTIONS" "$APP"
+fi

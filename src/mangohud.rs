44c416cd27500a8af25fbf97add808a90c9a4fd3
VoidArc Studio <chudybyczek2137@gmail.com>
2025-05-27 13:25:08 +0200

    Update mangohud.rs

--- src/mangohud.rs
+++ src/mangohud.rs
@@ -2,7 +2,7 @@ use std::env;
 
 pub fn init_mangohud() -> Result<(), Box<dyn std::error::Error>> {
     env::set_var("MANGOHUD", "1");
-    env::set_var("MANGOHUD_CONFIG_FILE", "/path/to/config/mangohud.conf");
+    env::set_var("MANGOHUD_CONFIG_FILE", "/home/user/.config/gameframe/mangohud.conf");
     println!("MangoHud włączony");
     Ok(())
 }

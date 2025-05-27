c74f0b10db9268ff27d1a53c79851f270b1c6d64
VoidArc Studio <chudybyczek2137@gmail.com>
2025-05-27 13:15:58 +0200

    steam.rs

--- src/steam.rs
+++ src/steam.rs
@@ -1,10 +1,13 @@
 use std::process::Command;
 
-pub fn launch_steam() -> Result<(), Box<dyn std::error::Error>> {
-    Command::new("steam")
-        .arg("-gamepadui")
+pub fn launch_application(app: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = app.split_whitespace();
+    let cmd = args.next().unwrap_or("steam");
+    let cmd_args: Vec<&str> = args.collect();
+    Command::new(cmd)
+        .args(cmd_args)
         .env("SDL_VIDEODRIVER", "wayland")
         .spawn()
-        .expect("Nie udało się uruchomić Steam");
+        .expect("Nie udało się uruchomić aplikacji");
     Ok(())
 }

54347fee7d02a613aa5e7e72f1005702272aaf1f
VoidArc Studio <chudybyczek2137@gmail.com>
2025-05-27 13:02:33 +0200

    vkbasalt.rs

---
+++ src/vkbasalt.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+pub fn init_vkbasalt() -> Result<(), Box<dyn std::error::Error>> {
+    env::set_var("ENABLE_VKBASALT", "1");
+    env::set_var("VKBASALT_CONFIG_FILE", "/path/to/config/vkbasalt.conf");
+    println!("vkBasalt włączony");
+    Ok(())
+}
+
+pub fn apply_post_processing<R: Renderer>(_renderer: &mut R, _frame: &mut R::Frame) -> Result<(), Box<dyn std::error::Error>> {
+    // Logika integracji z vkBasalt (przekazanie buforów Vulkan)
+    Ok(())
+}

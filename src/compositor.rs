0d4a6104712f5e558b0bec369c03d60db2ac3d91
VoidArc Studio <chudybyczek2137@gmail.com>
2025-05-27 13:12:34 +0200

    compositor.rs

--- src/compositor.rs
+++ src/compositor.rs
@@ -2,34 +2,46 @@ use smithay::{
     backend::renderer::Renderer,
     desktop::{Space, Window},
     reexports::wayland_server::Display,
-    wayland::compositor::CompositorState,
-    wayland::output::OutputManagerState,
+    wayland::{
+        compositor::CompositorState,
+        output::OutputManagerState,
+        shell::xdg::XdgShellState,
+    },
+    utils::Size,
 };
 
 pub struct GameFrameCompositor {
     space: Space<Window>,
     compositor_state: CompositorState,
     output_manager: OutputManagerState,
+    xdg_shell_state: XdgShellState,
     resolution: Size<i32, smithay::utils::Physical>,
+    quality: String,
 }
 
 impl GameFrameCompositor {
-    pub fn new(display: &mut Display) -> Self {
+    pub fn new(display: &mut Display, width: i32, height: i32, quality: String) -> Self {
         let compositor_state = CompositorState::new::<Self, _>(display, None);
         let output_manager = OutputManagerState::new_with_xdg_output::<Self>(display);
+        let xdg_shell_state = XdgShellState::new::<Self, _>(display, None);
         let space = Space::new();
 
         GameFrameCompositor {
             space,
             compositor_state,
             output_manager,
-            resolution: Size::from((1280, 720)),
+            xdg_shell_state,
+            resolution: Size::from((width, height)),
+            quality,
         }
     }
 
     pub fn resize(&mut self, size: Size<i32, smithay::utils::Physical>) {
         self.resolution = size;
-        // Spoofing rozdzielczości
         println!("Nowa rozdzielczość: {:?}", self.resolution);
+    }
+
+    pub fn quality(&self) -> &str {
+        &self.quality
     }
 }
